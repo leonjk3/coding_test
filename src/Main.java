de6f2e4fd47c50d87f9fd2d435633d1a6b58b32d
leon <leonjk3@kakao.com>
2023-04-02 00:30:11 +0900

    coding_test init

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(1, 1);
-        map.put(2, 0);
-        map.put(3, 0);
-
-        for (int i = 0; i < num; i++) {
-            int aNum = sc.nextInt();
-            int bNum = sc.nextInt();
-
-            int aVal = map.get(aNum);
-            int bVal = map.get(bNum);
-
-            map.put(aNum, bVal);
-            map.put(bNum, aVal);
-        }
-        for (Integer key : map.keySet()) {
-            if (map.get(key) == 1) {
-                System.out.println(key);
-                break;
-            }
-        }
-
+        System.out.println("Hello World!");
     }
 }
